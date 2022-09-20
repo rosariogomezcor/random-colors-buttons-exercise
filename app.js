dc1dabef5de666192c00da317af2ee0892fdad43
Rosario <rosariogomez639@gmail.com>
2022-09-20 07:58:28 -0300

    add stylesheet

---
+++ app.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < 50; i++) {
+    const newButtons = document.createElement('button'); 
+    newButtons.innerText = "CLICK"; 
+    document.querySelector('body').appendChild(newButtons); 
+}
